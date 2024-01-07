405ef2261a0d6fdb894d5cdd1c0d3ae48c321b08
KCui0327 <kenny.cui@mail.utoronto.ca>
2024-01-06 21:11:32 -0800

    Added Settings skeleton

--- src/src/component/SettingBody.tsx
+++ src/src/component/SettingBody.tsx
@@ -1,9 +1,32 @@
+import { Slider } from './Button';
 import './SettingBody.css'
 
 function SettingBody() {
     return (
         <div className='SettingBody'>
-            
+            <div className='Content'>
+                <div className='Title'>Supported Sites</div>
+                <div className='Names'>
+                    <div className='Press'>X (Twitter) </div>
+                    <Slider stateProp='off' />
+                </div>
+                <div className='Names'>
+                    <div className='Press'>Instagram </div>
+                    <Slider stateProp='off' />
+                </div>
+                <div className='Names'>
+                    <div className='Press'>Facebook </div>
+                    <Slider stateProp='off' />
+                </div>
+                <div className='Names'>
+                    <div className='Title'>Manual Scanning</div>
+                    <Slider stateProp='off' />
+                </div>
+                <div className='Names'>
+                    <div className='Title'>Night Mode</div>
+                    <Slider stateProp='off' />
+                </div>
+            </div>
         </div>
     );
 }
