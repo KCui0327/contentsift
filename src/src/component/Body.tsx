5323d76564a63e166ded014f2b6c754c63612cab
Aden Chan <adenchan@gmail.com>
2024-01-06 19:28:29 -0800

    updated main page

--- src/src/component/Body.tsx
+++ src/src/component/Body.tsx
@@ -2,7 +2,34 @@ import './Body.css'
 
 function Body() {
     return (
-        <div className='Body'>
+        <div className='body'>
+            <div className='header'>This Session</div>
+            <div className='info-box'>
+                <div className='table-heading'>Items Scanned</div>
+                <div className='table-value'>127</div>
+            </div>
+            <div className='info-box'>
+                <div className='table-heading'>Items Flagged</div>
+                <div className='table-value'>17</div>
+            </div>
+            <div className='info-box'>
+                <div className='table-heading'>Percent Flagged</div>
+                <div className='table-value'>12.7%</div>
+            </div>
+
+            <div className='header'>All Time</div>
+            <div className='info-box'>
+                <div className='table-heading'>Items Scanned</div>
+                <div className='table-value'>12783</div>
+            </div>
+            <div className='info-box'>
+                <div className='table-heading'>Items Flagged</div>
+                <div className='table-value'>489</div>
+            </div>
+            <div className='info-box'>
+                <div className='table-heading'>Percent Flagged</div>
+                <div className='table-value'>15.9%</div>
+            </div>
         </div>
     );
 }
